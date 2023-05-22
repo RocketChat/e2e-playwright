dbd417a6a0efadd2ecfb9a57b9b40b80544f7072
Janaina Coelho <janainacoelhodr@gmail.com>
2023-05-22 15:13:58 -0300

    fix: config

--- playwright.config.ts
+++ playwright.config.ts
@@ -53,10 +53,6 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
 
     /* Test against mobile viewports. */
     // {

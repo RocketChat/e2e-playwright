3786e6157911fb9940f6eacb15454bc0ecd3ca5c
Janaina Coelho <janainacoelhodr@gmail.com>
2023-05-22 14:43:44 -0300

    feat:create a private and a public team

---
+++ tests/EE/support/user.ts
@@ -0,0 +1,13 @@
+import { Page } from '@playwright/test';
+import loginPage from '../../locators/loginPage.json';
+
+export async function login(page: Page) {
+  await page.goto(`${process.env.CANDIDATE_EE}`);
+  await page
+    .getByPlaceholder(loginPage.fields.email)
+    .fill(`${process.env.USER_ADMIN}`);
+  await page
+    .getByLabel(loginPage.fields.password)
+    .fill(`${process.env.PASSWORD_ADMIN}`);
+  await page.getByRole('button', { name: loginPage.button.login }).click();
+}

6c84db1159a3966f1001cbb3e0a3b600c5ff3368
Jessica Souza <jessica_schelly@hotmail.com>
2023-06-04 17:13:57 -0300

    create discussion test

---
+++ tests/EE/admin/discussion/discussion.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect, request } from '@playwright/test';
+import { login } from '../support/user';
+import home from '../../../locators/home.json';
+import createDiscussion from '../../../locators/createDiscussion.json';
+import { deleteDiscussion, deleteDiscussionAPI } from './support/discussion';
+import { createChannelAPI, deleteChannel } from '../channel/support/channel';
+
+test.beforeEach(async ({ page, context }) => {
+  await createChannelAPI(context.request, 'discussion-channel'); 
+  await login(page);
+});
+
+test('Create a discussion', async ({ page, context }) => {
+  await page.locator(home.button.createNew).click();
+  await page
+    .getByTestId(home.dropdown.createNew)
+    .getByText(home.text.discussion)
+    .click();
+  await page
+    .getByPlaceholder(createDiscussion.placeholder.selectChannel)
+    .fill('discussion-channel-automation')
+  await page.getByRole('option', { name: 'discussion-channel', exact: true }).locator('div').first().click();
+  await page.getByPlaceholder(createDiscussion.placeholder.nameDiscussion).fill('discussionTestAutomation');
+  await page.getByRole('button', { name: createDiscussion.button.create }).click();
+
+  expect(
+    await page.getByRole('link', { name: 'discussionTestAutomation' }).isVisible()
+  );
+});
+
+test.afterEach(async ({ page, context }) => {
+  await deleteDiscussionAPI(context.request, 'discussionTestAutomation');
+  await deleteChannel(page, 'discussion-channel-automation');
+});
